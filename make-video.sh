c682e87326cddd7b81efbbc867e44c98a837a50c
David Kirk <dsak@bu.edu>
2020-08-21 20:29:14 -0500

    Added an argument for make-video to specify the time

--- make-video.sh
+++ make-video.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-ffmpeg -framerate 24 -pattern_type glob -i 'timelapse-photos/*.jpg' -c:v h264_omx -b:v 5000k -y timelapse-video.mp4
+time=${1:-12}
+
+ffmpeg -framerate 24 -pattern_type glob -i "timelapse-photos/*-${time}_00*.jpg" -c:v h264_omx -b:v 5000k -y timelapse-video-${time}_00.mp4
+
