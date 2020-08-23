913c32e3744073889528812cbc1272ef4b615e44
David Kirk <dsak@bu.edu>
2020-08-22 19:03:37 -0500

    Changed scripts to use current time in file names and directories, for automation purposes

--- make-video.sh
+++ make-video.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-time=${1:-12}
+# Generate filename from date and time
+imgnames=`date +"/home/pi/Timelapse-Photographer/timelapse-photos/%Y_%m_%d-photos/*.jpg"`
+vidname=`date +"/home/pi/Timelapse-Photographer/timelapse-videos/%Y_%m_%d-video.mp4"`
 
-ffmpeg -framerate 24 -pattern_type glob -i "timelapse-photos/*-${time}_00*.jpg" -c:v h264_omx -b:v 5000k -y timelapse-video-${time}_00.mp4
+# echo "$imgnames"
+# echo $vidname
 
+ffmpeg \
+	-framerate 24 \
+	-pattern_type glob \
+	-i "$imgnames" \
+	-c:v h264_omx \
+	-b:v 5000k \
+	-y \
+	"$vidname"
